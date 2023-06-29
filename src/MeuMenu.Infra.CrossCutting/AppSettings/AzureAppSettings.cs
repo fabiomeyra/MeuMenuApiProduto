e6c191bbfc8b9e4e9edd913cd33d5a8a9b6fb6f9
Fábio <fabiomeyra@outlook.com>
2023-06-29 17:35:57 -0300

    fix: resolvendo configuração azure no appsettings

---
+++ src/MeuMenu.Infra.CrossCutting/AppSettings/AzureAppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace MeuMenu.Infra.CrossCutting.AppSettings;
+
+public class AzureAppSettings : BaseAppSettings
+{
+    private string? _armazenamentoImagens;
+    private string? _nomePastaImagens;
+    public string? ArmazenamentoImagens
+    {
+        get => RetornaValorDescriptografado(_armazenamentoImagens);
+        set => _armazenamentoImagens = value;
+    }
+
+    public string? NomePastaImagens
+    {
+        get => RetornaValorDescriptografado(_nomePastaImagens);
+        set => _nomePastaImagens = value;
+    }
+}
